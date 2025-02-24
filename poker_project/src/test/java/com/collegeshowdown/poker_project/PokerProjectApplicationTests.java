e09b8d7d34b2e91de5df1a4cc6cb3fa455149214
Braeden Turner <braedenturner22@gmail.com>
2025-02-23 20:58:57 -0500

    implementing Spring framework

---
+++ poker_project/src/test/java/com/collegeshowdown/poker_project/PokerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collegeshowdown.poker_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
