5b72c287ebe6d4233ed7103de47c1b1e72bcc9a7
sunchipnacho <22717474+sunflsks@users.noreply.github.com>
2025-04-11 14:15:38 -0400

    formatting

--- backend/src/main/java/com/collegeshowdown/poker_project/runtime/ConnectedPlayer.java
+++ backend/src/main/java/com/collegeshowdown/poker_project/runtime/ConnectedPlayer.java
@@ -3,8 +3,8 @@ package com.collegeshowdown.poker_project.runtime;
 import com.collegeshowdown.poker_project.model.Player;
 
 public class ConnectedPlayer {
-    private final Player player;
-    private final Object connection; // TODO - figure this out
+    public final Player player;
+    public final Object connection; // TODO - figure this out
 
     public ConnectedPlayer(Player player, Object connection) {
         this.player = player;
